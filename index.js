8a8b2937eacfdec68aed06ec459ae1db491f0660
HarYor785 <haryor785@gmail.com>
2023-12-03 23:16:09 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const io = require("socket.io")(8800, {
+    cors: {
+        origin: "https://connectme-socialmedia.netlify.app"
+    }
+})
+
+let activeUsers = []
+
+io.on("connection", (socket)=>{
+    //add new user
+    socket.on('new-user-add', (newUserId)=>{
+        //if user is not addedd previuosly
+        if(!activeUsers.some((user)=> user.userId === newUserId)){
+            activeUsers.push({
+                userId: newUserId,
+                socketId: socket.id
+            })
+
+            console.log("Connected Users", activeUsers)
+        }
+        
+        io.emit('get-users', activeUsers)
+    })
+
+
+    socket.on("disconnect", ()=>{
+        activeUsers = activeUsers.filter((user)=> user.socketId !== socket.id)
+        console.log("User Disconnected", activeUsers)
+        io.emit('get-users', activeUsers)
+    })
+
+    //Send message
+    socket.on('send-message', (data)=>{
+        const {receiverId} = data
+        const user = activeUsers.find((user)=>user.userId === receiverId)
+        console.log("Sending from socket to : ", receiverId)
+        if(user){
+            io.to(user.socketId).emit('receive-message', data)
+        }
+    })
+
+    //Create a post
+    socket.on('friend-request', (data)=>{
+        const {id} = data
+        const user = activeUsers.find((user)=>user.userId === id)
+        console.log(data)
+        if(user){
+            io.to(user.socketId).emit('new-request', data)
+        }
+    })
+
+})
